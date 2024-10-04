84252d9bf530a6521afd6c736c2e92832bd43026
helodeepakji <helodeepakji@gmail.com>
2024-10-04 17:48:35 +0530

    include css

--- src/Home.js
+++ src/Home.js
@@ -1,47 +1,19 @@
 import { NavLink } from 'react-router-dom';
 import Card from './Card';
+import './Home.css';
 
-// import json
-
-[
-  {
-    title : 'test',
-    des : 'test',
-    image : 'dshjdj'
-  },
-  {
-    title : 'test',
-    des : 'test',
-    image : 'dshjdj'
-  },
-  {
-    title : 'test',
-    des : 'test',
-    image : 'dshjdj'
-  },
-  {
-    title : 'test',
-    des : 'test',
-    image : 'dshjdj'
-  },
-  {
-    title : 'test',
-    des : 'test',
-    image : 'dshjdj'
-  },
-  
-]
 function Home() {
   return (
     <div>
-      <div className='d-flex'>
+      <div className='d-flex card_design'>
 
-      // loop
+        <Card title="test" desc="testing testing" image="https://letsenhance.io/static/8f5e523ee6b2479e26ecc91b9c25261e/1015f/MainAfter.jpg" />
+        <Card title="test" desc="testing testing" image="https://letsenhance.io/static/8f5e523ee6b2479e26ecc91b9c25261e/1015f/MainAfter.jpg" />
+        <Card title="test" desc="testing testing" image="https://letsenhance.io/static/8f5e523ee6b2479e26ecc91b9c25261e/1015f/MainAfter.jpg" />
+        <Card title="test" desc="testing testing" image="https://letsenhance.io/static/8f5e523ee6b2479e26ecc91b9c25261e/1015f/MainAfter.jpg" />
+        <Card title="test" desc="testing testing" image="https://letsenhance.io/static/8f5e523ee6b2479e26ecc91b9c25261e/1015f/MainAfter.jpg" />
+        <Card title="test" desc="testing testing" image="https://letsenhance.io/static/8f5e523ee6b2479e26ecc91b9c25261e/1015f/MainAfter.jpg" />
 
-        <Card title="..." desc=".." image="..." /> 
-        
-      /end loop/
-      
       </div>
     </div>
   );
